ef0e90e88f8b1b01442ca5cba203baa9f20b27c8
siliconninja <theninja@thesilicon.ninja>
2020-05-24 17:30:18 -0400

    Reorganized code in main.c for Wombat

--- AdaLovelace2020/lego/main.c
+++ AdaLovelace2020/lego/main.c
@@ -12,6 +12,35 @@
 //LEFT MOTOR needs to have the black facing away from the wombat, while the RIGHT MOTOR needs to have the black
 //facing toward the wombat
 
+// INFO the robot should be in the start position.
+// For later (if needed): Recalculate distance to the ramp if it is in a different position.
+void move_to_ramp() {
+    forward_d(60, 2);
+}
+
+void move_antennae(){
+    // attenae
+    backward_t(60, 1.5);
+    right_t(60, 0.5);
+    backward_t(60, 1);
+    right_t(60, 0.5);
+}
+
+void move_toward_mountain() {
+    // moving toward the mountain
+    servo(arm, 600);
+    msleep(1000);
+    forward_t(30, 0.1);
+    msleep(1000);
+    servo(arm, 1200);
+    msleep(1000);
+    forward_t(30, 0.1);
+    msleep(1000);
+    servo(arm, arm_down);
+    msleep(1000);
+    forward_t(30, 0.1);
+}
+
 int main()
 {
     printf("Hello World\n");
@@ -23,29 +52,13 @@ int main()
     enable_servos();
     printf("Hello World\n");
 
-    // attenae
-    /*backward_t(60, 1.5);
-    right_t(60, 0.5);
-    backward_t(60, 1);
-    right_t(60, 0.5);*/
-
-    // moving toward the mountain
-    /*servo(arm, 600);
-    msleep(1000);
-    forward_t(30, 0.1);
-    msleep(1000);
-    servo(arm, 1200);
-    msleep(1000);
-    forward_t(30, 0.1);
-    msleep(1000);
-    servo(arm, arm_down);
-    msleep(1000);
-    forward_t(30, 0.1);*/
+    move_to_ramp();
+    //move_toward_mountain();
 
     msleep(5000);
     printf("Hello World\n");
 
-    drive_w_arm(5, arm_down);
+    //drive_w_arm(5, arm_down);
 
     disable_servos();
     return 0;
