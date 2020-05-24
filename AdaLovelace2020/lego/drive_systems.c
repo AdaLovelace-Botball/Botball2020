228daf55c2695e56ba68daf1165e82e18a72403b
siliconninja <theninja@thesilicon.ninja>
2020-05-24 17:31:34 -0400

    Implement forward_d and backward_d functions on Wombat

--- AdaLovelace2020/lego/drive_systems.c
+++ AdaLovelace2020/lego/drive_systems.c
@@ -9,6 +9,8 @@
 #include <servos&motors.h>
 #include "variables.h"
 
+#include "gyro.h"
+
 //This file includes functions used for movement involving wheels
 //Functions include: forward_t, backward_t, left_t, right_t, forward_d, backward_d, left_d, right_d
 
@@ -42,10 +44,22 @@ void left_t(int speed, float seconds) {
     right_t(-speed, seconds);
 }
 
+// INFO this uses the gyroscope on the Wallaby so it's more accurate
+// speed is in ticks/second (0 to 1500)
+// speed cannot be negative because we want it to have positive time
+// TODO write a unit test to reject negative values
+// (Because of the mav function in simple_drive_with_gyro:
+// https://files.kipr.org/wallaby/wallaby_doc/group__motor.html#gabd36f01986c363f70d86c7a768ae1348)
 void forward_d(int speed, float inches) {
+    simple_drive_with_gyro(speed, inches/speed);
 }
 
+// INFO this uses the gyroscope on the Wallaby so it's more accurate
+// speed is in ticks/second (0 to 1500)
+// speed cannot be negative because we want it to have positive time
+// TODO write a unit test to reject negative values
 void backward_d(int speed, float inches) {
+    simple_drive_with_gyro(-speed, inches/speed);
 }
 
 void left_d(int speed, float inches) {
